9afffc931ec7db4c3a264acfdf6b9fb8fe7f6225
Radish Logic <radishlogic01@gmail.com>
2018-03-08 22:55:46 +0800

    Initial code
    Arduino Uno Code for determining Schmitt Trigger Voltage Levels of the Arduino Uno.

---
+++ ArduinoSchmittTriggerLevels/ArduinoSchmittTriggerLevels.ino
@@ -0,0 +1,12 @@
+#define PIN_INPUT 2
+#define PIN_OUTPUT 3
+
+void setup() {
+  pinMode(PIN_INPUT, INPUT);
+  pinMode(PIN_OUTPUT, OUTPUT);
+}
+
+void loop() {
+  boolean inputValue = digitalRead(PIN_INPUT);
+  digitalWrite(PIN_OUTPUT, inputValue);
+}
